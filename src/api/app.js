bafe631fe0ec3f58d05a538fab0ad90feda535d7
ririyad <rasheduliriyad@gmail.com>
2021-07-31 17:15:00 +0600

    tested the first api

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
 const app = express();
+const bookRouter = express.Router();
 const port = process.env.PORT || 3300;
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = {hello: "This is from API layer"};
+    res.json(response);
+  });
+
+app.use('/api/', bookRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to the bookreads API');
 });
