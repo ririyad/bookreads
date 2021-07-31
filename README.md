0191d54ef8ce3b52ce405c5de0587ba462a8697f
ririyad <rasheduliriyad@gmail.com>
2021-07-31 17:26:37 +0600

    restrcturing project

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ A personal book management system inspired by Goodreads
 - Postman for API testing
 - Hosting/Deployment ?
 
+## Initial Start
+
+To start the project, run`npm start`
+then the `nodemon` will be started.s
+
 
--- src/api/src/app.js
+++ src/api/src/app.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 3300;
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = {hello: "This is from API layer"};
+    const response = {hello: "This is from book layer"};
     res.json(response);
   });
+
+  bookRouter.route('/authors')
+  .get((req, res) => {
+    const response = {author: "Hello from author route"};
+    res.json(response);
+  })
 
 app.use('/api/', bookRouter);
 app.get('/', (req, res) => {
